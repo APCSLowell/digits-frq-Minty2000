b85d20ebc0b915c3b1e06adca55d154727b4a362
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-04-10 10:34:49 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -17,8 +17,8 @@ public class Digits
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		    for (int i=0; i<digitList.size()-1; i++){
-      if (digitList.get(i)>=digitList.get(i+1))
-        return false;
+      if (digitList.get(i)>=digitList.get(i+1)){
+        return false;}
 		    }
     return true;
 
